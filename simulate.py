e8243b397eb413bc18decc8a62ede59b14d16cc0
Phillip Nguyen <pnguyen4711@gmail.com>
2021-02-22 21:18:10 -0500

    storing sensor values (numpy)

--- simulate.py
+++ simulate.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
@@ -11,9 +12,11 @@ bodyId = p.loadURDF("body.urdf")    # the robot
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate("body.urdf")
+backLegSensorValues = np.zeros(10000)
 for i in range(0, 1000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
 
 p.disconnect()
+print(backLegSensorValues)
