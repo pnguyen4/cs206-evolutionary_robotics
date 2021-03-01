fbcb62be2fb32c961462289caa97bcab752a1e44
Phillip Nguyen <pnguyen4711@gmail.com>
2021-03-01 16:36:23 -0500

    correct joint settings

--- generate.py
+++ generate.py
@@ -11,11 +11,11 @@ def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[1, 1, 1])
     pyrosim.Send_Joint(name="Torso_Leg1", parent="Torso", child="FrontLeg",
-                       type="revolute", position="0.5 0 0")
-    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, 0.5], size=[1, 1, 1])
+                       type="revolute", position="0.5 0 1.0")
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
     pyrosim.Send_Joint(name="Torso_Leg2", parent="Torso", child="BackLeg",
-                       type="revolute", position="-0.5 0 0")
-    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, 0.5], size=[1, 1, 1])
+                       type="revolute", position="-0.5 0 1.0")
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
     pyrosim.End()
 
 
