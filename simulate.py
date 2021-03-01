6d07efcdec5704aa0d83478afd4d58cdd4ea83fc
Phillip Nguyen <pnguyen4711@gmail.com>
2021-03-01 16:37:27 -0500

    tippy toe robot

--- simulate.py
+++ simulate.py
@@ -3,12 +3,13 @@ import pybullet as p
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import time
+import math
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0, 0, -9.8)
 planeId = p.loadURDF("plane.urdf")  # the floor
-bodyId = p.loadURDF("body.urdf")    # the robot
+robot = p.loadURDF("body.urdf")    # the robot
 p.loadSDF("world.sdf")
 
 pyrosim.Prepare_To_Simulate("body.urdf")
@@ -18,6 +19,21 @@ for i in range(0, 1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robot,
+        jointName="Torso_Leg1",  # front leg
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=math.pi/6.0,
+        maxForce=500
+    )
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex=robot,
+        jointName="Torso_Leg2",  # back leg
+        controlMode=p.POSITION_CONTROL,
+        targetPosition=-math.pi/6.0,
+        maxForce=500
+    )
     time.sleep(1/60)
 
 np.save('data/back_leg_sensor_values.npy', backLegSensorValues)
